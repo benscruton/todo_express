223f4c8888fe5a06f3efb8e6961df4e3abfdb4b2
benscruton <benjamin.scruton@gmail.com>
2023-08-02 23:00:44 -0500

    Delete route for all todos (was intended as dev-only)

--- server/routes/todo.routes.js
+++ server/routes/todo.routes.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const {todoController} = require("../controllers");
 const {confirmPassphrase} = require("../config/middleware");
 
-// Disable in production
-router.route("/")
-  .get(todoController.all)
-
 router.route("/:todoId")
   .get(confirmPassphrase, todoController.findById)
   .put(confirmPassphrase, todoController.updateById)
